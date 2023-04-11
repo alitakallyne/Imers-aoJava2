ae94f78e0884ec5f64f51e900460999345ebd6a2
alitakallyne <alytakallyne@gmail.com>
2023-04-11 19:57:24 -0300

    Aula03

--- src/App.java
+++ src/App.java
@@ -1,34 +1,47 @@
 import java.io.InputStream;
-import java.net.URI;
 import java.net.URL;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
-import java.util.Map;
 
-public class App {
+
+public class App { 
  
     public static void main(String[] args) throws Exception {
-       // System.out.println("Hello, World!");
 
         //Fazer uma conexão http e  buscar os tops 250 filmes.
-        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
-        URI endereco = URI.create(url);
-        var client = HttpClient.newHttpClient();
-        var request = HttpRequest.newBuilder(endereco).GET().build();
-        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-        String body = response.body();
+       
+        API api = API.NASA_APOD;
+
+        String url = api.getUrl();
+        ExtratorDeConteudo extrator = api.getExtrator();
+                
+        var http = new ClienteHttp();
+        String json = http.buscaDados(url);
+
+        //exibir e manipular dados
+       
+        List<Conteudo> conteudos = extrator.ExtraiConteudo(json);
+
+        var geradora = new GeradoraDeFigurinhas();
+
+        for(int i = 0; i < 3 ; i++){
+            Conteudo conteudo = conteudos.get(i);
+
+            InputStream inputStream = new URL(conteudo.urlImagem()).openStream();
+            String nomeArquivo = "./saida/" +conteudo.titulo() +".png";
+
+            geradora.criarFigura(inputStream, nomeArquivo);
+
+            System.out.println(conteudo.titulo());
+            System.out.println();
+
+        }
         
 
-        //Extrair só os dados que interessam(titulo,poster,classifição)
-        var parser = new JsonParser();
-        List<Map<String,String>> listaDeFilmes = parser.parse(body);
+
+
         //System.out.println(listaDeFilmes.size()); 
-        //exibir e manipular os dados
-        
-        for (Map<String,String> filme : listaDeFilmes) {
+        //exibir e manipular os dados Aula02
+        /*  for (Map<String,String> filme : listaDeFilmes) {
             String urlImage = filme.get("image");
             String titulo = filme.get("title");
 
@@ -49,6 +62,7 @@ public class App {
                //// System.out.print("\uD83D\uDC99 💙️");
             }
             System.out.println();
-        }
+        } */
+       
     }
 }
---
+++ src/ExtratorDeConteudoDaNasa.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class ExtratorDeConteudoDaNasa implements ExtratorDeConteudo {
+    
+    public List<Conteudo> ExtraiConteudo (String json){
+        
+         //Extrair só os dados que interessam(titulo,poster,classifição)
+         var parser = new JsonParser();
+         List<Map<String,String>> listaDeAtributos = parser.parse(json);
+
+         // Usando Stream para add em uma lista
+        return  listaDeAtributos.stream()
+            .map(atributos -> new Conteudo (atributos.get("title"),atributos.get("url")))
+            .toList();
+         /* 
+         //Popular lista de conteudos
+         List<Conteudo> Conteudos = new ArrayList<>();
+        
+         for(Map<String,String> atributos : listaDeAtributos){
+             
+            String titulo = atributos.get("title");
+            String urlImagem = atributos.get("url");
+            var conteudo = new Conteudo(titulo, urlImagem);
+
+            Conteudos.add(conteudo);
+         }
+
+        return Conteudos; */
+    }
+}
